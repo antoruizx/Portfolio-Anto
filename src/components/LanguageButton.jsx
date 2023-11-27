c5d81fc0fcda02dcbe4a5fa5dcf21234736f38b0
antoruizx <83556067+antoruizx@users.noreply.github.com>
2023-11-27 04:00:55 -0300

    botones idioma

--- src/components/LanguageButton.jsx
+++ src/components/LanguageButton.jsx
@@ -5,13 +5,18 @@ import '../styles/LanguageButton.css';
 function LanguageButton() {
     const [t, i18n] = useTranslation("global");
   return (
-    <div>
+    <div >
         <h1 className='text-white'>{t("header.hello-world")}</h1>
         <br />
 
-        <div>
-            <button onClick={() => i18n.changeLanguage("es")} className='text-white bg-gradient-to-b from-purple-600 to-purple-900 px-3 py-3 my-4 mx-auto flex items-center rounded-md hover:scale-110 duration-300 font-bold'>ES</button> 
-            <button onClick={() => i18n.changeLanguage("en")} className='text-white bg-gradient-to-b from-purple-600 to-purple-900 px-3 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300 font-bold'>EN</button>
+        <div className='container padre'>
+            <div>
+                <button onClick={() => i18n.changeLanguage("es")} className='text-white bg-gradient-to-b from-purple-600 to-purple-900 px-3 py-3 my-4 mx-auto flex items-center rounded-md hover:scale-110 duration-300 font-bold es '>ES</button> 
+            </div>
+
+            <div>
+                <button onClick={() => i18n.changeLanguage("en")} className='text-white bg-gradient-to-b from-purple-600 to-purple-900 px-3 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300 font-bold en'>EN</button> 
+            </div>
 
         </div>
 
